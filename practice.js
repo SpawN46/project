bed9d2acad2392b5b8b128d90cc02845aca6ea1f
SpawN <steroid1988@yandex.ru>
2021-12-01 17:55:22 +0500

    first commit

---
+++ practice.js
@@ -0,0 +1,169 @@
+'use strict'
+
+var names = 'SpawN';
+let age = 33;
+let info = true;
+var net = undefined;
+let so = null;
+let structure = {};
+var mass = [];
+
+console.log(names);
+console.log(age);
+console.log(info);
+console.log(net);
+console.log(so);
+
+let structue_2 = {
+    name: 'John',
+    age: 11,
+    adult: true
+};
+
+console.log(structue_2.name);
+console.log(structue_2['age']);
+
+var mass_2 = [
+    'image.bmp',
+    'secret.jpg',
+    'surpise.png'
+];
+
+console.log(mass_2[2]);
+
+let comp_1 = '33';
+let comp_2 = 33;
+
+console.log(comp_1 == comp_2);
+console.log(comp_1 === comp_2);
+
+//alert('We have the window here');
+//console.log(confirm('Are you here now?'));
+//console.log(prompt('How old are you?'));
+
+let incr = 10, decr = 10;
+
+incr++
+decr--
+
+console.log(incr, decr);
+console.log(++incr, ++decr);
+
+let st = true;
+let st_2 = false;
+
+console.log(st && st_2);
+console.log(st && !st_2);
+console.log(st || st_2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,93 @@
+'use strict';
+
+var number = 5;
+var string = "Hello!";
+var boolean = true;
+null;
+undefined;
+var object = {};
+
+console.log(4/0);
+console.log("string" * 5);
+
+let something;
+console.log(something);
+
+let person = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(person.name);
+console.log(person["name"])
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+console.log(arr[0]);
+
+
+//alert('Hello World');
+
+
+//let  answer = confirm('Are you okay?');
+
+//console.log(answer);
+
+
+//let age = prompt('How old are you?', '33');
+
+//console.log(age);
+//console.log(typeof(age));
+
+
+//let age = +prompt('How old are you?', '33');
+
+//console.log(age);
+//console.log(typeof(age));
+
+
+//console.log('air' + 'land');
+//console.log('air' + 5);
+
+
+let incr = 10, decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+console.log('2' == 2);
+console.log('2' === 2);
+
+let isChecked = true, isClosed = false;
+
+console.log(isChecked && isClosed);
+console.log(isChecked && !isClosed);
+console.log(isChecked || isClosed);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
